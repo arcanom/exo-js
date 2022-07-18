6dd05751034caad11beb9547e4af0ec15f453170
Corentin Duriez <corentinduriez22@gmail.com>
2022-07-18 14:15:44 +0200

    Code mort retiré

--- exo5.js
+++ exo5.js
@@ -9,9 +9,6 @@ const  tauxInteret = prompt("Votre taux d'interet ? ");
 var prompt = require("prompt-sync")();
 const  duree = prompt("Votre durée ? ");
 
-let pourcent =  tauxInteret/100
-console.log(pourcent)
-console.log(Math.pow(1+pourcent,duree))
 let capitalFinal;
 capitalFinal = Math.round(parseInt(capital) * (Math.pow(1+pourcent,duree)));
 console.log(`Votre capital final s'éleve à ${capitalFinal}`)
