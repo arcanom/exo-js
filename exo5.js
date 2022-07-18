bf495583577bc9ccab1a3632c45e7ec01d56e7a1
Corentin Duriez <corentinduriez22@gmail.com>
2022-07-18 14:01:50 +0200

    Toujours faux

--- exo5.js
+++ exo5.js
@@ -10,8 +10,8 @@ var prompt = require("prompt-sync")();
 const  duree = prompt("Votre durée ? ");
 
 let montantInteret ;
-montantInteret =  Math.round( capital * (tauxInteret/100) * duree);
-console.log(`Votre montant d'intéret s'élévera de  ${montantInteret}`)
+montantInteret =   Math.round(Math.pow(tauxInteret,duree));
+console.log(`Votre montant d'intéret s'élévera de  ${montantInteret}`) 
 let capitalFinal;
 capitalFinal = parseInt(capital) + montantInteret;
 console.log(`Votre capital final s'éleve à ${capitalFinal}`)
